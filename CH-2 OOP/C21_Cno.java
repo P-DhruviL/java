c7e5d886186d53247afad859575d30657e6bfe43
Dhruvil-prajapati-16 <151436113+Dhruvil-prajapati-16@users.noreply.github.com>
2024-03-14 15:49:05 +0530

    #

--- CH-2 OOP/C21_Cno.java
+++ CH-2 OOP/C21_Cno.java
@@ -1,12 +1,12 @@
-public class C21_Cno
+public class C21_Cno // class
 {
    
-void displayA()
+void displayA()  // method 
 {
     System.out.println("hey");
 }    
 
-void displayB()
+void displayB() // method 
 {
 System.out.println("Hello_word");
 }
@@ -14,8 +14,8 @@ System.out.println("Hello_word");
 public static void main(String args[])
 {
 
-C21_Cno obja = new C21_Cno();
-obja.displayA();
+C21_Cno obja = new C21_Cno(); // className obj_name = new Classname()
+obja.displayA(); // obj_name.Method_name
 obja.displayB();
 
 }
